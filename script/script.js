1b92013c1e3b233d52e7acb4113654367f87596e
Nguyen Van Viet <nviet03052004@gmail.com>
2024-10-20 13:03:37 +0700

    Test new routing

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
     const inviteButton = document.getElementById('inviteButton')
     
     inviteButton.addEventListener('click', () => {
-        window.location.href = '../main/main.html'
+        window.location.href = 'Women-day/main/main.html'
     })
     checkScreenSize()
     window.addEventListener('resize', checkScreenSize)
@@ -17,7 +17,7 @@ function checkScreenSize() {
             confirmButtonText: 'OK',
         }).then((result) => {
             if (result.isConfirmed) {
-                window.location.href = '../mobile_waring/mobile_warning.html'
+                window.location.href = 'Women-day/mobile_waring/mobile_warning.html'
             }
         })
     } else {
